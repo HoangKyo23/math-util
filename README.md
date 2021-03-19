49198358af4704a7a1fe87226c6daa9f8ee1c53d
HoangKyo23 <hoangnlh23@gmail.com>
2021-03-19 14:53:09 +0700

    Add a new README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+## Welcome to Math utility repository.
+this repo offers various math methods used as a helper for your project.
---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author Hoang Kyo
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("xin chao");
+        System.out.println("alo alo github");
+    }
+    
+}
